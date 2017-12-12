7a4cb003e8d26c67863387b0ae84355076b23a5d
Kristin Yin <kristinyin@hotmail.com>
2017-12-11 22:24:32 -0500

    optimize runtime

--- generate/generate.py
+++ generate/generate.py
@@ -91,7 +91,7 @@ def randomize(title):
             threshold = 0.80
             met = False
             numberOfTriedArticles = 0
-            maxTries = 70
+            maxTries = 50
             bestLine = None
             bestScore = 0
 
